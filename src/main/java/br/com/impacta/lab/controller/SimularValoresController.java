68a4b9b06a133b92f8ea7b9e7777ccca14e7c171
Paulo Marques <pauloedums@gmail.com>
2021-05-15 18:51:59 -0300

    Adding entities and algorithm logic for the products discounts

--- src/main/java/br/com/impacta/lab/controller/SimularValoresController.java
+++ src/main/java/br/com/impacta/lab/controller/SimularValoresController.java
@@ -1,58 +1,100 @@
 package br.com.impacta.lab.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.impacta.lab.entities.Product;
+import br.com.impacta.lab.entities.TypePayment;
+
 @RestController
 @RequestMapping("/simular")
 public class SimularValoresController {
 
+	public String product = "";
+	public String payment = "";
+	public double productValue = 0.0;
+	public double productValueAfterDiscount = 0.0;
+	public String message = "";
+
 	@GetMapping
 	public ResponseEntity<String> simularValores(@RequestParam(name="codigoProduto") int codigoProduto,
 			@RequestParam(name="codTipoPagamento") int codTipoPagamento) {
-		/*
-		 * Exemplo de chamada:
-		 * https://localhost:8080/simular?codigoProduto=1&codTipoPagamento=2
-		 * 
-		 * Elabore um algoritmo para calcular o valor final de um produto com base em seu valor REAL
-		 *  e a condição de pagamento.
-		 *  
-		 *  A escolha do produto e sua condição de pagamento da-se através de seus códigos, faça um 
-		 *  algoritmo também para realizar a escolha de cada um dos itens a partir de seus códigos.
-		 *  
-		 *  Tabela de produto
-		 *  
-		 *  CodigoProduto	descrição do Produto		Valor do Produto
-		 * 	1				Camisa						70.00
-		 * 	2				Shorts						57.50
-		 * 	3				Meia						9.99
-		 * 	4				Toca						35.00
-		 * 	5				Luvas						19.50
-		 * 
-		 * -----------------------------------------------------------------
-		 * 
-		 * Tabela de condição de pagamento
-		 * 
-		 * 	CodTipoPagamento		descrição da condição de pagamento
-		 * 	1						A vista no dinheiro com 10% de desconto
-		 * 	2						A vista no cartão de crédito  com 5% de desconto
-		 * 	3						Em duas parcelas sem nenhum desconto
-		 * 	4						Em três vezes com 10% de juros
-		 * 
-		 * OBS: Considerar sempre os descontos ou juros do valor do produto
-		 * A resposta do exercicio deve seguir o seguinte formato:
-		 * 
-		 * <descrição do produto> sendo pago <descrição da condição de pagamento> custará <valor final do produto> reais
-		 * 
-		 * Ex.: Camisa sendo pago A vista no dinheiro com 10% de desconto custará 63.0 reais
-		 * 
-		 */
+
+		/* Lista dos produtos */
+		List<Product> products = new ArrayList<>();
+			products.add(new Product(1, "Camisa", 70.00));
+			products.add(new Product(2, "Shorts", 57.50));
+			products.add(new Product(3, "Meia", 9.99));
+			products.add(new Product(4, "Toca", 35.00));
+			products.add(new Product(5, "Luvas", 19.50));
+
+		/* Lista dos tipos de pagamento */
+		List<TypePayment> paymentTypes = new ArrayList<>();
+			paymentTypes.add(new TypePayment(1, "A vista no dinheiro com 10% de desconto"));
+			paymentTypes.add(new TypePayment(2, "A vista no cartão de crédito  com 5% de desconto"));
+			paymentTypes.add(new TypePayment(3, "Em duas parcelas sem nenhum desconto"));
+			paymentTypes.add(new TypePayment(4, "Em três vezes com 10% de juros"));
 		
-		
-		return ResponseEntity.ok("Hello world !");
+		/* condicional para um código válido */
+		if(codigoProduto != 0 && codTipoPagamento != 0) {
+			
+			/* condicional para um código válido, pórem fora do range de produtos */
+			if(codigoProduto > products.size()) {
+				message = "Não foi encontrado nenhum produto, por favor tente novamente.";
+			}
+			/* condicional para um código válido, pórem fora do range de tipos de pagamento */
+			else if(codTipoPagamento > paymentTypes.size()) {
+				message = "Não foi encontrado nenhum tipo de pagamento, por favor tente novamente.";
+			}
+			/* condicional para um código válido e um tipo de pagamento válido */
+			else {
+				Product resultProduct = products.stream()
+				.filter(p -> p.getCode() == codigoProduto).findFirst().get();
+				
+				paymentTypes.stream().forEach(p -> {
+					payment = p.getDescription();
+					product = resultProduct.getDescription();
+					productValue = resultProduct.getValue();
+
+					switch(codTipoPagamento) {
+						case 1: 
+							productValueAfterDiscount = (productValue - (productValue * 0.10));
+							break;
+						case 2: 
+							productValueAfterDiscount = (productValue - (productValue * 0.05));
+							break;
+						case 3:
+							productValueAfterDiscount = productValue;
+							break;
+						case 4: 
+							productValueAfterDiscount = (productValue * 0.10) + productValue;
+							break;
+						default: break;
+					}
+				});
+				message = product + " sendo pago " + payment + " custará " + productValueAfterDiscount + " reais";
+			}
+		}
+		/* condicional para um código de produto inválido */
+		else if(codigoProduto == 0 || codigoProduto <= 0){
+			message = "Por favor, digite um código de produto válido";
+		}
+		/* condicional para um tipo de pagamento inválido */
+		else if(codTipoPagamento == 0 || codTipoPagamento <= 0){
+			message = "Por favor, digite um tipo de pagamento válido";
+		}		
+		/* condicional para valores inválidos ou sem inventário */
+		else {
+			message = "Sua pesquisa não retornou nenhum dado, por favor tente novamente.";
+		}
+
+		return ResponseEntity.ok(message);
 	}
 	
 }
---
+++ src/main/java/br/com/impacta/lab/entities/Product.java
@@ -0,0 +1,36 @@
+package br.com.impacta.lab.entities;
+
+public class Product {
+    
+    private int code;
+    private String description;
+    private double value;
+
+    public Product(int code, String description, double value){
+        this.code = code;
+        this.description = description;
+        this.value = value;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    
+}
